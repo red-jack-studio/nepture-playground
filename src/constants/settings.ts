35b12c1ba1817565ab19bbf396dd74d9aab21aeb
ZNX <henrique.baronceli@gmail.com>
2024-03-31 23:26:02 +1300

    improve comments on settings.js

--- src/constants/settings.ts
+++ src/constants/settings.ts
@@ -1,6 +1,8 @@
 /*
-    Here we define the main Nepture API server address and the
-    engine (or endpoint) we are going to use with the playground.
+    Here we define the main Nepture API server address, the
+    engine (or endpoint) we are going to use with the playground,
+    and the context factor (the amount of messages used for
+    context with each request).
 
     Available Engine Endpoints:
 
@@ -9,7 +11,14 @@
 
     If you are using a local Nepture API server, make sure to
     declare the correct port.
+
+    Default Configuration:
+
+    export const NeptureAPI = `http://localhost:5000/api`;
+    export const NeptureEngineEndpoint = `/playground/gpt35t`;
+    export const ContextFactor = '10';
 */
+
 export const NeptureAPI = `http://localhost:5000/api`;
 export const NeptureEngineEndpoint = `/playground/gpt35t`;
 export const ContextFactor = '10';
